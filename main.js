37710af028b0e96099a362989dc23c82fe16d9e8
Teehxw <112659668+Teehxw@users.noreply.github.com>
2022-10-14 13:41:12 -0600

    Array

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 // Array Visualizer
 
+//HTML Variables
+let containerE1 =  document.getElementById("container");
+
+
 // Global Variable 
-let myData = []
+let myData = [400, 500, 250, 100, 550, 300];
+
+// Display Data 
+drawArray();
+
+function drawArray (){
+    let outputStr = "";
+    for (let i = 0; i< myData.length; i++){
+       outputStr += `<div>${myData[i]}</div>`
+    }
+    containerE1.innerHTML = outputStr; 
+}
