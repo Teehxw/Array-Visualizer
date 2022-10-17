64f6afa26c74c1fa8e792cb4548369748d1c8a7f
Teehxw <112659668+Teehxw@users.noreply.github.com>
2022-10-17 13:42:59 -0600

    idk man

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let containerE1 =  document.getElementById("container");
 
 
 // Global Variable 
-let myData = [400, 500, 250, 100, 550, 300];
+let myData = [600, 350, 250, 550, 100, 300, 600, 100, 100];
 
 // Display Data 
 drawArray();
@@ -13,7 +13,7 @@ drawArray();
 function drawArray (){
     let outputStr = "";
     for (let i = 0; i< myData.length; i++){
-       outputStr += `<div>${myData[i]}</div>`
+       outputStr += `<div style = "height:${myData[i]}px"> </div>`
     }
     containerE1.innerHTML = outputStr; 
 }
